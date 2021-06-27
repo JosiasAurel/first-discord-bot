9e0d3fced4bc5b14cb5def4ef607c9e9340654ee
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-27 16:25:42 +0100

    use discord.py find() utility with predicate

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ bot = discord.Client()  # instance a new client
 
 @bot.event
 async def on_ready():  # event client has established connection with discord API
-    for guild in bot.guilds:
-        if guild.name == my_guild:
-            break
+    guild = discord.utils.find(lambda g: g.name == my_guild, bot.guilds)
 
     print(f"{bot.user} is in guild {guild.name} ID: {guild.id} ")
     members = "\n - ".join([member.name for member in guild.members])
