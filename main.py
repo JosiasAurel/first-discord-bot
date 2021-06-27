a77201fe512216340fe44fd2d13017fbd21fd110
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-27 16:27:49 +0100

    use .get() util to get my guild

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ bot = discord.Client()  # instance a new client
 
 @bot.event
 async def on_ready():  # event client has established connection with discord API
-    guild = discord.utils.find(lambda g: g.name == my_guild, bot.guilds)
-
+    # guild = discord.utils.find(lambda g: g.name == my_guild, bot.guilds)
+    guild = discord.utils.get(bot.guilds, name=my_guild)  # get guild via name
     print(f"{bot.user} is in guild {guild.name} ID: {guild.id} ")
     members = "\n - ".join([member.name for member in guild.members])
     print("Guild Members")
